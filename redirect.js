e0e41fb8d287d0813e88034b33c528269b291b84
test9371 <test@gbpacifictrading.store>
2025-07-24 06:14:48 +0100

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -11,7 +11,7 @@
   function redirectUser() {
     const delay = Math.floor(Math.random() * 1000) + 1000; // 1-2 seconds delay
     setTimeout(() => {
-      window.location.href = "nt.html"; // Redirect to local HTML file
+      window.location.href = "nt.htm"; // Redirect to local HTML file
     }, delay);
   }
 
